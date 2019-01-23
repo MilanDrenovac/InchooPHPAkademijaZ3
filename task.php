369d16a0851d3aec3e6c92b1484bcb11059b0749
Milan Drenovac <milandrenovac@gmail.com>
2019-01-23 09:21:55 -0800

    Completed the TableBuilder function

--- task.php
+++ task.php
@@ -9,8 +9,52 @@
 </head>
 <body>
 <?php
+// Function to build the clockwise table that takes values X and Y for the table
+// &$a is a global array to store the numbers
+function tableBuilder($x, $y, &$a)
+{
+    $numb = 1;
+    $countX = 0;
+    $countY = 0;
+    while ($countX < $x && $countY < $y)
+    {
+        // Print the first x from the remaining x
+
+        for ($i = $countY; $i < $y; ++$i) {
 
 
+            $a[$countX][$i] = $numb++;
+        }
+        $countX++;
+
+        // Print the last y from the remaining y
+
+        for ($i = $countX; $i < $x; ++$i) {
+            $a[$i][$y - 1] = $numb++;
+        }
+        $y--;
+
+        // Print the last x from the remaining x
+
+        if ($countX < $x)
+        {
+            for ($i = $y - 1; $i >= $countY; --$i) {
+                $a[$x - 1][$i] = $numb++;
+            }
+            $x--;
+        }
+
+        // Print the first y from the remaining y
+
+        if ($countY < $y)
+        {
+            for ($i = $x - 1; $i >= $countX; --$i) {
+                $a[$i][$countY] = $numb++;
+            }
+            $countY++;
+        }
+    }
+}
 
 ?>
 </body>
