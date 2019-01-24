c76871b5b2bbdc8f7969f3a914b5ce2103092de7
Milan Drenovac <milandrenovac@gmail.com>
2019-01-23 23:51:51 -0800

    Number checking and float

--- index.php
+++ index.php
@@ -71,6 +71,13 @@ function tableBuilder($x, $y, &$a, &$class)
 }
 $x = $_POST['x'];
 $y = $_POST['y'];
+// Sanitation Checks
+if (!is_numeric($x) || $x <=0 ||!is_numeric($y) || $y <=0 ) {
+    echo "<p>This is not a valid number, please input valid numbers<p>";
+    exit();
+}
+$x = round($x ,0);
+$y = round($y ,0);
 
 tableBuilder($x, $y, $a, $class);
 
